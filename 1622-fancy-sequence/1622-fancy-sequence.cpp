772948a1b4dfc32226f69c34b1be068f90c774e4
Hariom Prajapati <hariompup17@gmail.com>
2026-08-06 09:50:45 +0530

    Time: 79 ms (38.79%), Space: 184.6 MB (57.88%) - LeetHub

---
+++ 1622-fancy-sequence/1622-fancy-sequence.cpp
@@ -0,0 +1,40 @@
+class Fancy {
+public:
+    const long long MOD = 1e9 + 7;
+
+    vector<long long> vec;
+    long long mul = 1;
+    long long add = 0;
+
+    long long modInverse(long long x){
+        long long res = 1, p = MOD - 2;
+        while(p){
+            if(p & 1) res = (res * x) % MOD;
+            x = (x * x) % MOD;
+            p >>= 1;
+        }
+        return res;
+    }
+
+    Fancy() {}
+
+    void append(int val) {
+        long long inv = modInverse(mul);
+        long long stored = ((val - add + MOD) % MOD * inv) % MOD;
+        vec.push_back(stored);
+    }
+
+    void addAll(int inc) {
+        add = (add + inc) % MOD;
+    }
+
+    void multAll(int m) {
+        mul = (mul * m) % MOD;
+        add = (add * m) % MOD;
+    }
+
+    int getIndex(int idx) {
+        if(idx >= vec.size()) return -1;
+        return (vec[idx] * mul % MOD + add) % MOD;
+    }
+};
